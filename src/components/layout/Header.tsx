ccb2a574cc174f73afe402c941fd5ef1af534f8c
Leandro Thiago Ribeiro <111009157+LeandroTRibeiro@users.noreply.github.com>
2024-07-07 15:54:44 -0300

    final project

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,10 @@
-import logo from "@/assets/image/logo.png";
 import { Bell, MessageCircle } from "lucide-react"
 import { ModeToggle } from "../features/ModeToggle";
 import { UserMenu } from "./UserMenu";
 
 export const Header = () => {
     return (
-        <header className="flex justify-between items-center sticky top-0 z-50 w-full h-14 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="flex justify-between items-center sticky top-0 z-50 w-full h-56 py-5 border-b-2  bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <h1 className="text-2xl font-light">AVA<span className="font-extrabold">UNESC</span></h1>
             <nav className="flex items-center gap-5">
                 <Bell className="cursor-pointer hover:scale-110 transition-all duration-300 ease-in-out" />
--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
-import { BadgeAlert, BadgeCheck, ThumbsDown, ThumbsUp, FileVideo2,  Files, FileText } from "lucide-react";
+import { BadgeAlert, BadgeCheck, ThumbsDown, ThumbsUp, FileVideo2,  Files, FileText, Mail, PhoneCall, Filter, Check } from "lucide-react";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion";
 import { BreadcrumMenu } from "./BreadCrumbMain"
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "../ui/hover-card";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import prof from "@/assets/image/professora.png";
+import { Calendar } from "../ui/calendar";
+import { Input } from "../ui/input";
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "../ui/menubar";
 
 export const Main = () => {
+
+    const dateStrings = [
+        "Tue Jul 02 2024 19:46:28 GMT-0300 (Horário Padrão de Brasília)",
+        "Tue Jul 09 2024 19:46:28 GMT-0300 (Horário Padrão de Brasília)",
+        "Tue Jul 16 2024 19:46:28 GMT-0300 (Horário Padrão de Brasília)",
+        "Tue Jul 23 2024 19:46:28 GMT-0300 (Horário Padrão de Brasília)"
+    ];
+    
+    const dateObjects = dateStrings.map(dateString => new Date(dateString));
+
+    const [date, setDate] = useState<Date[] | undefined>(dateObjects);
 
     const aulas = [
         {
@@ -290,19 +305,58 @@ export const Main = () => {
         FileVideo2,  
         Files, 
         FileText
-    }
+    };
+
+    console.log(date);
+    
 
     return (
         <main>
             <BreadcrumMenu />
-            <div className="h-full flex">
-                <div>
-                    <h1>materia</h1>
-                    <p>descricao</p>
-                    <p>professora</p>
+            <div className="h-full flex gap-5">
+                <div className="flex flex-col gap-5 w-[400px]">
+                    <h1 className="font-bold text-4xl">Design de Interação</h1>
+                    <p className="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum atque fuga molestias, voluptate quisquam esse dolorem eligendi possimus.</p>
+                    <div className="flex items-center gap-2">
+                        <Avatar>
+                            <AvatarImage src={prof} />
+                            <AvatarFallback>LL</AvatarFallback>
+                        </Avatar>
+                        <h2 className="text-base font-semibold">Profº Ma Leila Laís Gonçalves</h2>
+                        <Mail className="text-blue-500 cursor-pointer transition-all duration-300 ease-in-out hover:scale-110" />
+                        <PhoneCall className="text-green-500 cursor-pointer transition-all duration-300 ease-in-out hover:scale-110" size={21}/>
+                    </div>
+                    <div className="">
+                        <Calendar 
+                            mode="multiple"
+                            selected={date}
+                            onSelect={setDate}
+                            className="rounded-md border shadow w-fit"
+                        />
+                    </div>
                 </div>
-                <div className="flex-1 flex justify-center h-full">
-                    <Accordion type="single" collapsible className="w-[500px]">
+                <div className="flex-1 h-full">
+                    <div className="w-[700px] flex justify-end items-center mb-5">
+                        <Input
+                            className=""
+                            placeholder="Pesquisar Aulas"
+                        />
+                        <Menubar className="absolute">
+                            <MenubarMenu>
+                                <MenubarTrigger className="cursor-pointer"><Filter size={20} /></MenubarTrigger>
+                                <MenubarContent>
+                                    <MenubarItem className="cursor-pointer">Aulas Online</MenubarItem>
+                                    <MenubarItem className="cursor-pointer">Aulas Presenciais</MenubarItem>
+                                    <MenubarItem className="cursor-pointer">Aulas por Mês</MenubarItem>
+                                    <MenubarItem className="cursor-pointer">Atividades</MenubarItem>
+                                    <MenubarItem className="cursor-pointer">Avaliações</MenubarItem>
+                                    <MenubarItem className="cursor-pointer flex justify-between">Todas <Check size={18} className="text-green-600" /></MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+                        </Menubar>
+                        
+                    </div>
+                    <Accordion type="single" collapsible className="w-[700px]">
                         {aulas.map(item => (
                             <AccordionItem value={item.title} key={item.id}>
                                 <AccordionTrigger>
